872a867b4cb9743ba3d48e79f2bdf2aaa203956e
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2024-02-16 08:01:04 +0000

    nvim: Add keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,24 @@
 local keymap = vim.keymap
 
+local opts = { noremap = true, silent = true }
+
 -- Delete a word backwards
 keymap.set("n", "dw", 'vb"_d')
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- ToggleTerm
+keymap.set("n", "<Leader>t", "<cmd>ToggleTerm size=50 direction=vertical<CR>")
+keymap.set("n", "<Leader>ts", "<cmd>ToggleTerm direction=horizontal<CR>")
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
+keymap.set("n", "<C-k>", function()
+  vim.diagnostic.goto_prev()
+end, opts)
