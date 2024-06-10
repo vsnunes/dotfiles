b59709aaa3fc389fc35675825800f31f2a7967c9
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2024-06-10 16:35:33 +0100

    reafactor(nvim): simplify nvim-cmp config

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,15 +1,11 @@
 -- nvim-cmp configs
 return {
-  -- customize nvim-cmp configs
-  -- Use <tab> for completion and snippets (supertab)
-  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
     "L3MON4D3/LuaSnip",
     keys = function()
       return {}
     end,
   },
-  -- then: setup supertab in cmp
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
@@ -17,44 +13,38 @@ return {
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local luasnip = require("luasnip")
       local cmp = require("cmp")
 
-      -- This is reaaaally not easy to setup :D
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        -- Disables inserting selected suggestion when pressing ENTER
-        ["<CR>"] = cmp.config.disable,
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          -- If it's a snippet then jump between fields
-          if luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          -- otherwise if the completion pop is visible then complete
-          elseif cmp.visible() then
-            cmp.confirm({ select = false })
-          -- if the popup is not visible then open the popup
-          elseif has_words_before() then
-            cmp.complete()
-          -- otherwise fallback
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
+      opts.snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      }
+      opts.window = {
+        completion = { cmp.config.window.bordered(), col_offset = 3, side_padding = 1 },
+        documentation = cmp.config.window.bordered(),
+      }
+
+      opts.mapping = {
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<C-S-f>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      }
+
+      opts.sources = {
+        { name = "path" }, -- file paths
+        { name = "nvim_lsp", keyword_length = 1, priority = 10 }, -- from language server
+        { name = "crates", keyword_length = 1, priority = 10 },
+        { name = "luasnip", keyword_length = 1, priority = 7 }, -- for lua users
+        { name = "nvim_lsp_signature_help", priority = 8 }, -- display function signatures with current parameter emphasized
+        { name = "nvim_lua", keyword_length = 1, priority = 8 }, -- complete neovim's Lua runtime API such vim.lsp.*
+        { name = "buffer", keyword_length = 1, priority = 5 }, -- source current buffer
+        { name = "calc" }, -- source for math calculation
+      }
     end,
   },
 }
