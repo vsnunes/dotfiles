d297f6bea343c4c3531a6cbe0cfec98a6f8b3656
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2026-02-13 20:28:27 +0000

    Fix nvim treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require'nvim-treesitter.config'.setup {
     ensure_installed = { "lua", "markdown", "vim" },
     sync_install = false,
     auto_install = true,
