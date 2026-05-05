4505a2df357955d72d6e81961c27503009260dce
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2026-05-05 22:23:49 +0100

    Add lazyvim copilot settings

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local copilot_enabled = false
+
+local function toggle_copilot()
+  if copilot_enabled then
+    vim.cmd("Copilot disable")
+    print("Copilot Disabled")
+    copilot_enabled = false
+  else
+    vim.cmd("Copilot enable")
+    print("Copilot Enabled")
+    copilot_enabled = true
+  end
+end
+
+vim.keymap.set("n", "<leader>ct", toggle_copilot, { desc = "Toggle Copilot" })
---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,9 @@
+return {
+  "zbirenbaum/copilot.lua",
+  filetypes = {
+    ["*"] = false,
+  },
+
+  suggestion = { enabled = false },
+  panel = { enabled = false },
+}
